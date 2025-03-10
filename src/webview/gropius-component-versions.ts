279605f5a76c88740f7a9b7cfe7ba1c192dd2445
Kristinapn <krisi17899@gmail.com>
2025-03-10 16:34:35 +0100

    Merge branch 'main' of https://github.com/ccims/gropius-vsc

--- src/webview/gropius-component-versions.ts
+++ src/webview/gropius-component-versions.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import GropiusComponentVersions from './GropiusComponentVersions.vue';
+import './global.css';
 
 const app = createApp(GropiusComponentVersions);
 app.mount('#app');
