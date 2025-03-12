e0f024e3f84b606b0c512e0fe4f77f0526082a14
KKlimentov1618 <kklimentov1618@gmail.com>
2025-03-12 17:06:58 +0100

    Component Issues View Bug Solved

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const CLIENT_ID = "your-client-id"; // Replace with your client ID
-export const CLIENT_SECRET = "your-client-secret"; // Replace with your client secret
+export const CLIENT_ID = "ebd7bfaf-7136-4549-b0b3-17a8b16a104b"; // Replace with your client ID
+export const CLIENT_SECRET = "9755d60ecd5ee8d94f10b293cd8bba"; // Replace with your client secret
 export const API_URL = "http://localhost:4200"; // Replace with your API URL
