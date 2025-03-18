3d87dbb73562ad27777beb450e3212d72a937550
Kristinapn <krisi17899@gmail.com>
2025-03-18 17:09:44 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const CLIENT_ID = "24239da8-c703-41a2-9ad5-93a131ec18a6"; // Replace with your client ID
-export const CLIENT_SECRET = "6b0ad8d9a140db53d9a338ecbb45ef"; // Replace with your client secret
+export const CLIENT_ID = "your-client-id"; // Replace with your client ID
+ export const CLIENT_SECRET = "your-client-secret";  // Replace with your client secret
 export const API_URL = "http://localhost:4200"; // Replace with your API URL
