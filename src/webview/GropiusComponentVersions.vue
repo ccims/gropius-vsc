e23be4d9dd4fb7a0ec833d46165e64b5b9c300c3
Kristinapn <krisi17899@gmail.com>
2025-04-30 10:17:27 +0200

    Update GropiusComponentVersions.vue

--- src/webview/GropiusComponentVersions.vue
+++ src/webview/GropiusComponentVersions.vue
@@ -531,7 +531,7 @@ body {
 /* Flex wrapper for right-aligning the graph button */
 .version-toolbar {
   display: flex;
-  justify-content: flex-end;
+  /*  justify-content: flex-end; */
   margin: 4px 8px;
   align-items: center;
 }
